0d99f7fccd7c6b3c1eab76501bb57377687c4da9
DEJA1 <allowestq@gmail.com>
2023-01-14 17:20:07 +0100

    add splash screen

--- app/src/main/java/com/example/gameenc/MainActivity.kt
+++ app/src/main/java/com/example/gameenc/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.rememberNavController
 import com.example.gameenc.navigation.SetupNavGraph
